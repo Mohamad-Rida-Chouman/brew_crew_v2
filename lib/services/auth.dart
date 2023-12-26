9bbf483f03444136bab9f2c79e10491ff25ac04d
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-12-26 12:17:40 +0200

    Backend <> Added Try Block to Anonymous Signin

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,6 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   // Anonymous sign in
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user!;
+    } catch (e) {}
+  }
 
   // Sign in with email and pass
 
