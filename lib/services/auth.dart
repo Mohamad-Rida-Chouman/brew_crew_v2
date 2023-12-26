6712e0629389f4d33f2966d99f7853fa508ba74a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-12-26 11:45:04 +0200

    Backend <> Created Authentication Service Class

---
+++ lib/services/auth.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  // Anonymous sign in
+
+  // Sign in with email and pass
+
+  // Register with email and pass
+
+  // Sign out
+}
